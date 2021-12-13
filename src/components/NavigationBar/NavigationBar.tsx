113d0a5fddca75ef1cb113448683337db61e3dda
Lionnarta <13518128@std.stei.itb.ac.id>
2021-12-13 13:35:11 +0700

    setup, init navbar component

---
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './NavigationBar.css'
+
+interface NavProps {
+    
+}
+
+const NavigationBar = (props: NavProps) => {
+
+    const [searchState, setSearchState] = useState(false);
+
+    return (
+        <div>
+            Tes
+        </div>
+    )
+};
+
+export default NavigationBar;
